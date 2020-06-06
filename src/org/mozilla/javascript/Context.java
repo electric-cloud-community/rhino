2e139b137bec55a4b74c2201937a32c643e80eba
Anders Wallgren <awallgren@cloudbees.com>
2020-06-06 14:49:04 -0700

    PATCH3

--- src/org/mozilla/javascript/Context.java
+++ src/org/mozilla/javascript/Context.java
@@ -290,6 +290,12 @@ public class Context
      * @since 1.7 Release 3
      */
     public static final int FEATURE_V8_EXTENSIONS = 14;
+	
+	/**
+     * Enables non-ECMA style conversion from an object to a boolean under
+     * object control via getDefaultValue().
+     */
+    public static final int FEATURE_ALLOW_OBJECT_AS_BOOLEAN = 15;
 
     public static final String languageVersionProperty = "language version";
     public static final String errorReporterProperty   = "error reporter";
